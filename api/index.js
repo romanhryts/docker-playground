a008323e96c29a608533182159bce51d23e4a401
Roman Hrytsyshyn <romangry.2004@gmail.com>
2023-12-03 18:10:02 +0100

    DOCKER | Monsterlessons
    ✅  Lessons 1-12

---
+++ api/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('./src/config');
+const Connector = require('./src/connector');
+
+const app = express();
+
+const DB = {
+  post: {
+    _model: null,
+    name: 'Post',
+    schema: new mongoose.Schema({ title: String }),
+    model() {
+      this._model ||= mongoose.model(this.name, this.schema);
+      return this._model;
+    }
+  }
+}
+
+app.get('/:model', async (req, res) => {
+  const [data] = await DB[req.params.model].model().find();
+  res.json(data);
+});
+
+const startServer = () => {
+  app.listen(config.PORT, () => {
+    console.log(`API is running on port ${config.PORT}`);
+  });
+}
+
+Connector
+  .mongodb(config.MONGODB_URL)
+  .on('error', console.log)
+  .once('open', startServer);
